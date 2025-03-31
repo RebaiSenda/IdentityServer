9bee2001cfb6cba26165c81a414e1fc51488b575
RebaiSenda <rebaisenda93@gmail.com>
2025-03-31 19:28:57 +0200

    minor changes

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,18 +1,21 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
-        };
+       [
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+       ];
+
 
     public static IEnumerable<ApiScope> ApiScopes =>
         [
-        new ApiScope(name: "resevrationApi", displayName: "ReservationAPI")
+        new ApiScope(name: "resevrationApi", displayName: "ReservationAPI"),
+        new ApiScope(name: "KataReservation.Api", displayName: "ReservationAPI"),
         ];
 
     public static IEnumerable<Client> Clients =>
@@ -32,6 +35,27 @@ public static class Config
 
             // scopes that client has access to
             AllowedScopes = { "resevrationApi" }
+        },
+                // interactive ASP.NET Core Web App
+        new Client
+        {
+            ClientId = "web",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+            
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:7002/signin-oidc"},
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:7002/signout-callback-oidc" },
+
+            AllowedScopes =
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                 "KataReservation.Api"
+            }
         }
        ];
 
--- IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
+++ IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207c17131016a2857cfec56f52e420084846ea92")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
